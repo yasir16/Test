cbc68cf2582e94f5d2e18a017ce17cf4a591bf9c
yasyas <rayas143120@gmail.com>
2021-06-14 18:54:15 +0700

    Refactoring

--- anagram.go
+++ anagram.go
@@ -5,41 +5,29 @@ import (
 	"reflect"
 )
 
-func isAnagram(str1 string, str2 string) bool {
+func mappingStr(str string) map[string]int {
+	mapping := make(map[string]int)
+	for _, word := range str {
+		count := mapping[string(word)]
+		if count == 0 {
+			mapping[string(word)] = 1
+		} else {
+			mapping[string(word)] = count + 1
+		}
+	}
+	return mapping
+}
+func isAnagram(str1, str2 string) bool {
 	if len(str1) != len(str2) {
 		return false
 	}
-
-	hash := make(map[string]int)
-	hash2 := make(map[string]int)
-
-	for _, r := range str1 {
-		j := hash[string(r)]
-		if j == 0 {
-			hash[string(r)] = 1
-		} else {
-			hash[string(r)] = j + 1
-		}
-	}
-	for _, r := range str2 {
-		j := hash2[string(r)]
-		if j == 0 {
-			hash2[string(r)] = 1
-		} else {
-			hash2[string(r)] = j + 1
-		}
-	}
-	isOK := false
-	for _, value := range hash {
-		if value%2 != 0 {
-			isOK = false
-		}
-	}
-	if !reflect.DeepEqual(hash, hash2) && isOK == false {
+	mappingstr1 := mappingStr(str1)
+	mappingstr2 := mappingStr(str2)
+	if !reflect.DeepEqual(mappingstr1, mappingstr2) {
 		return false
 	}
-
 	return true
+
 }
 func main() {
 	fmt.Println(isAnagram("asuu", "usaa"))
